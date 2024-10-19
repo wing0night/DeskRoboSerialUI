2cef4863a005e88467a49add790e614a364cfe87
NIGHT <3210103847@zju.edu.cn>
2024-10-20 01:10:01 +0800

    add image, serial communication

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 import sys
 
 from PySide6.QtWidgets import QApplication, QWidget
+import webbrowser
+# for serial communication
+import serial
+import time
 
 # Important:
 # You need to run the following command to generate the ui_form.py file
@@ -14,6 +18,106 @@ class Widget(QWidget):
         super().__init__(parent)
         self.ui = Ui_Widget()
         self.ui.setupUi(self)
+        self.set_signals()
+        self.open_serialPort()
+    
+    def open_serialPort(self):
+        # 设置串口参数
+        port = 'COM5'  # 串口名称，例如 'COM3' 或 '/dev/ttyS0'
+        baudrate = 115200  # 波特率
+        bytesize = serial.EIGHTBITS  # 数据位
+        parity = serial.PARITY_NONE  # 奇偶校验位
+        stopbits = serial.STOPBITS_ONE  # 停止位
+        # 打开串口
+        self.ser = serial.Serial(port, baudrate, bytesize, parity, stopbits, timeout=1)
+    
+    # 设置信号与槽
+    def set_signals(self):
+        self.ui.github.clicked.connect(self.go_to_github)
+        self.ui.eye_blink.clicked.connect(self.eye_blink)
+        self.ui.eye_sad.clicked.connect(self.eye_sad)
+        self.ui.eye_anger.clicked.connect(self.eye_anger)
+        self.ui.eye_surprise.clicked.connect(self.eye_surprise)
+        self.ui.eye_right.clicked.connect(self.eye_right)
+        self.ui.head_center.clicked.connect(self.head_center)
+        self.ui.head_nod.clicked.connect(self.head_nod)
+        self.ui.head_shake.clicked.connect(self.head_shake)
+        self.ui.head_roll.clicked.connect(self.head_roll)
+    
+    # 跳转到 GitHub 查看源代码
+    def go_to_github(self):
+        url = 'https://github.com/wing0night/DeskRoboSerialUI.git'
+        webbrowser.open(url)
+    
+    def eye_blink(self):
+        # # 检查串口是否打开
+        # if self.ser.is_open:
+        #     print("串口已打开")
+        # try:
+        # 发送数据
+        data_to_send = "eye_blink"  # 要发送的数据
+        self.ser.write(data_to_send.encode())  # 发送数据，需要先编码为字节
+        print("数据已发送")
+        # 等待一段时间，以便看到发送的数据
+        time.sleep(1)
+        # finally:
+        #     # 关闭串口
+        #     self.ser.close()
+        #     print("串口已关闭")
+    def eye_sad(self):
+        data_to_send = "eye_sad"  # 要发送的数据
+        self.ser.write(data_to_send.encode())  # 发送数据，需要先编码为字节
+        print("数据已发送")
+        # 等待一段时间，以便看到发送的数据
+        time.sleep(1)
+    def eye_anger(self):
+        data_to_send = "eye_anger"  # 要发送的数据
+        self.ser.write(data_to_send.encode())  # 发送数据，需要先编码为字节
+        print("数据已发送")
+        # 等待一段时间，以便看到发送的数据
+        time.sleep(1)
+    def eye_surprise(self):
+        data_to_send = "eye_surprise"  # 要发送的数据
+        self.ser.write(data_to_send.encode())  # 发送数据，需要先编码为字节
+        print("数据已发送")
+        # 等待一段时间，以便看到发送的数据
+        time.sleep(1)
+    def eye_right(self):
+        data_to_send = "eye_right"  # 要发送的数据
+        self.ser.write(data_to_send.encode())  # 发送数据，需要先编码为字节
+        print("数据已发送")
+        # 等待一段时间，以便看到发送的数据
+        time.sleep(1)
+    def eye_left(self):
+        data_to_send = "eye_left"  # 要发送的数据
+        self.ser.write(data_to_send.encode())  # 发送数据，需要先编码为字节
+        print("数据已发送")
+        # 等待一段时间，以便看到发送的数据
+        time.sleep(1)
+    def head_center(self):
+        data_to_send = "head_center"  # 要发送的数据
+        self.ser.write(data_to_send.encode())  # 发送数据，需要先编码为字节
+        print("数据已发送")
+        # 等待一段时间，以便看到发送的数据
+        time.sleep(1)
+    def head_nod(self):
+        data_to_send = "head_nod"  # 要发送的数据
+        self.ser.write(data_to_send.encode())  # 发送数据，需要先编码为字节
+        print("数据已发送")
+        # 等待一段时间，以便看到发送的数据
+        time.sleep(1)
+    def head_shake(self):
+        data_to_send = "head_shake"  # 要发送的数据
+        self.ser.write(data_to_send.encode())  # 发送数据，需要先编码为字节
+        print("数据已发送")
+        # 等待一段时间，以便看到发送的数据
+        time.sleep(1)
+    def head_roll(self):
+        data_to_send = "head_roll"  # 要发送的数据
+        self.ser.write(data_to_send.encode())  # 发送数据，需要先编码为字节
+        print("数据已发送")
+        # 等待一段时间，以便看到发送的数据
+        time.sleep(1)
 
 
 if __name__ == "__main__":
--- ui_form.py
+++ ui_form.py
@@ -24,28 +24,28 @@ class Ui_Widget(object):
             Widget.setObjectName(u"Widget")
         Widget.resize(643, 566)
         Widget.setStyleSheet(u"background-color: rgb(255, 248, 232);")
-        self.pushButton = QPushButton(Widget)
-        self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(370, 40, 101, 41))
-        self.pushButton.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+        self.head_center = QPushButton(Widget)
+        self.head_center.setObjectName(u"head_center")
+        self.head_center.setGeometry(QRect(370, 40, 101, 41))
+        self.head_center.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
 "font: 12pt \"Modern No. 20\";\n"
 "color: rgb(0, 0, 0);")
-        self.pushButton_2 = QPushButton(Widget)
-        self.pushButton_2.setObjectName(u"pushButton_2")
-        self.pushButton_2.setGeometry(QRect(490, 40, 101, 41))
-        self.pushButton_2.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+        self.head_nod = QPushButton(Widget)
+        self.head_nod.setObjectName(u"head_nod")
+        self.head_nod.setGeometry(QRect(490, 40, 101, 41))
+        self.head_nod.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
 "font: 12pt \"Modern No. 20\";\n"
 "color: rgb(0, 0, 0);")
-        self.pushButton_3 = QPushButton(Widget)
-        self.pushButton_3.setObjectName(u"pushButton_3")
-        self.pushButton_3.setGeometry(QRect(370, 100, 101, 41))
-        self.pushButton_3.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+        self.head_shake = QPushButton(Widget)
+        self.head_shake.setObjectName(u"head_shake")
+        self.head_shake.setGeometry(QRect(370, 100, 101, 41))
+        self.head_shake.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
 "font: 12pt \"Modern No. 20\";\n"
 "color: rgb(0, 0, 0);")
-        self.pushButton_4 = QPushButton(Widget)
-        self.pushButton_4.setObjectName(u"pushButton_4")
-        self.pushButton_4.setGeometry(QRect(490, 100, 101, 41))
-        self.pushButton_4.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+        self.head_roll = QPushButton(Widget)
+        self.head_roll.setObjectName(u"head_roll")
+        self.head_roll.setGeometry(QRect(490, 100, 101, 41))
+        self.head_roll.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
 "font: 12pt \"Modern No. 20\";\n"
 "color: rgb(0, 0, 0);")
         self.listView = QListView(Widget)
@@ -60,40 +60,40 @@ class Ui_Widget(object):
         self.listView_4.setObjectName(u"listView_4")
         self.listView_4.setGeometry(QRect(20, 20, 281, 441))
         self.listView_4.setStyleSheet(u"background-color: rgb(103, 70, 54);")
-        self.pushButton_5 = QPushButton(Widget)
-        self.pushButton_5.setObjectName(u"pushButton_5")
-        self.pushButton_5.setGeometry(QRect(50, 270, 101, 41))
-        self.pushButton_5.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+        self.eye_blink = QPushButton(Widget)
+        self.eye_blink.setObjectName(u"eye_blink")
+        self.eye_blink.setGeometry(QRect(50, 270, 101, 41))
+        self.eye_blink.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
 "font: 12pt \"Modern No. 20\";\n"
 "color: rgb(0, 0, 0);")
-        self.pushButton_6 = QPushButton(Widget)
-        self.pushButton_6.setObjectName(u"pushButton_6")
-        self.pushButton_6.setGeometry(QRect(170, 270, 101, 41))
-        self.pushButton_6.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+        self.eye_sad = QPushButton(Widget)
+        self.eye_sad.setObjectName(u"eye_sad")
+        self.eye_sad.setGeometry(QRect(170, 270, 101, 41))
+        self.eye_sad.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
 "font: 12pt \"Modern No. 20\";\n"
 "color: rgb(0, 0, 0);")
-        self.pushButton_7 = QPushButton(Widget)
-        self.pushButton_7.setObjectName(u"pushButton_7")
-        self.pushButton_7.setGeometry(QRect(170, 330, 101, 41))
-        self.pushButton_7.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+        self.eye_surprise = QPushButton(Widget)
+        self.eye_surprise.setObjectName(u"eye_surprise")
+        self.eye_surprise.setGeometry(QRect(170, 330, 101, 41))
+        self.eye_surprise.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
 "font: 12pt \"Modern No. 20\";\n"
 "color: rgb(0, 0, 0);")
-        self.pushButton_8 = QPushButton(Widget)
-        self.pushButton_8.setObjectName(u"pushButton_8")
-        self.pushButton_8.setGeometry(QRect(50, 330, 101, 41))
-        self.pushButton_8.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+        self.eye_anger = QPushButton(Widget)
+        self.eye_anger.setObjectName(u"eye_anger")
+        self.eye_anger.setGeometry(QRect(50, 330, 101, 41))
+        self.eye_anger.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
 "font: 12pt \"Modern No. 20\";\n"
 "color: rgb(0, 0, 0);")
-        self.pushButton_9 = QPushButton(Widget)
-        self.pushButton_9.setObjectName(u"pushButton_9")
-        self.pushButton_9.setGeometry(QRect(50, 390, 101, 41))
-        self.pushButton_9.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+        self.eye_right = QPushButton(Widget)
+        self.eye_right.setObjectName(u"eye_right")
+        self.eye_right.setGeometry(QRect(50, 390, 101, 41))
+        self.eye_right.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
 "font: 12pt \"Modern No. 20\";\n"
 "color: rgb(0, 0, 0);")
-        self.pushButton_10 = QPushButton(Widget)
-        self.pushButton_10.setObjectName(u"pushButton_10")
-        self.pushButton_10.setGeometry(QRect(170, 390, 101, 41))
-        self.pushButton_10.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+        self.eye_left = QPushButton(Widget)
+        self.eye_left.setObjectName(u"eye_left")
+        self.eye_left.setGeometry(QRect(170, 390, 101, 41))
+        self.eye_left.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
 "font: 12pt \"Modern No. 20\";\n"
 "color: rgb(0, 0, 0);")
         self.textEdit = QTextEdit(Widget)
@@ -103,7 +103,7 @@ class Ui_Widget(object):
         self.listView_3 = QListView(Widget)
         self.listView_3.setObjectName(u"listView_3")
         self.listView_3.setGeometry(QRect(50, 50, 211, 192))
-        self.listView_3.setStyleSheet(u"border-image: url(:/new/prefix1/robot.png);\n"
+        self.listView_3.setStyleSheet(u"border-image: url(\"images/robot.png\");\n"
 "background-color: rgba(255, 255, 255, 0);")
         self.pushButton_11 = QPushButton(Widget)
         self.pushButton_11.setObjectName(u"pushButton_11")
@@ -127,46 +127,46 @@ class Ui_Widget(object):
         self.label_2.setGeometry(QRect(230, 490, 71, 21))
         self.label_2.setStyleSheet(u"font: 10pt \"Microsoft YaHei UI\";\n"
 "color: rgb(0, 0, 0);")
-        self.pushButton_13 = QPushButton(Widget)
-        self.pushButton_13.setObjectName(u"pushButton_13")
-        self.pushButton_13.setGeometry(QRect(460, 480, 61, 31))
-        self.pushButton_13.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+        self.open = QPushButton(Widget)
+        self.open.setObjectName(u"open")
+        self.open.setGeometry(QRect(460, 480, 61, 31))
+        self.open.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
 "font: 12pt \"Modern No. 20\";\n"
 "color: rgb(0, 0, 0);")
-        self.pushButton_14 = QPushButton(Widget)
-        self.pushButton_14.setObjectName(u"pushButton_14")
-        self.pushButton_14.setGeometry(QRect(560, 480, 61, 31))
-        self.pushButton_14.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+        self.close = QPushButton(Widget)
+        self.close.setObjectName(u"close")
+        self.close.setGeometry(QRect(560, 480, 61, 31))
+        self.close.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
 "font: 12pt \"Modern No. 20\";\n"
 "color: rgb(0, 0, 0);")
-        self.pushButton_15 = QPushButton(Widget)
-        self.pushButton_15.setObjectName(u"pushButton_15")
-        self.pushButton_15.setGeometry(QRect(520, 520, 101, 31))
-        self.pushButton_15.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+        self.github = QPushButton(Widget)
+        self.github.setObjectName(u"github")
+        self.github.setGeometry(QRect(520, 520, 101, 31))
+        self.github.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
 "font: 12pt \"Modern No. 20\";\n"
 "color: rgb(0, 0, 0);")
         self.listView.raise_()
-        self.pushButton.raise_()
-        self.pushButton_2.raise_()
-        self.pushButton_3.raise_()
-        self.pushButton_4.raise_()
+        self.head_center.raise_()
+        self.head_nod.raise_()
+        self.head_shake.raise_()
+        self.head_roll.raise_()
         self.listView_2.raise_()
         self.listView_4.raise_()
-        self.pushButton_5.raise_()
-        self.pushButton_6.raise_()
-        self.pushButton_7.raise_()
-        self.pushButton_8.raise_()
-        self.pushButton_9.raise_()
-        self.pushButton_10.raise_()
+        self.eye_blink.raise_()
+        self.eye_sad.raise_()
+        self.eye_surprise.raise_()
+        self.eye_anger.raise_()
+        self.eye_right.raise_()
+        self.eye_left.raise_()
         self.textEdit.raise_()
         self.listView_3.raise_()
         self.pushButton_11.raise_()
         self.pushButton_12.raise_()
         self.label.raise_()
         self.label_2.raise_()
-        self.pushButton_13.raise_()
-        self.pushButton_14.raise_()
-        self.pushButton_15.raise_()
+        self.open.raise_()
+        self.close.raise_()
+        self.github.raise_()
 
         self.retranslateUi(Widget)
 
@@ -175,22 +175,22 @@ class Ui_Widget(object):
 
     def retranslateUi(self, Widget):
         Widget.setWindowTitle(QCoreApplication.translate("Widget", u"Widget", None))
-        self.pushButton.setText(QCoreApplication.translate("Widget", u"head-center", None))
-        self.pushButton_2.setText(QCoreApplication.translate("Widget", u"head_nod", None))
-        self.pushButton_3.setText(QCoreApplication.translate("Widget", u"head_shake", None))
-        self.pushButton_4.setText(QCoreApplication.translate("Widget", u"head_roll", None))
-        self.pushButton_5.setText(QCoreApplication.translate("Widget", u"eye_blink", None))
-        self.pushButton_6.setText(QCoreApplication.translate("Widget", u"eye_sad", None))
-        self.pushButton_7.setText(QCoreApplication.translate("Widget", u"eye_surprise", None))
-        self.pushButton_8.setText(QCoreApplication.translate("Widget", u"eye_anger", None))
-        self.pushButton_9.setText(QCoreApplication.translate("Widget", u"eye_right", None))
-        self.pushButton_10.setText(QCoreApplication.translate("Widget", u"eye_left", None))
+        self.head_center.setText(QCoreApplication.translate("Widget", u"head_center", None))
+        self.head_nod.setText(QCoreApplication.translate("Widget", u"head_nod", None))
+        self.head_shake.setText(QCoreApplication.translate("Widget", u"head_shake", None))
+        self.head_roll.setText(QCoreApplication.translate("Widget", u"head_roll", None))
+        self.eye_blink.setText(QCoreApplication.translate("Widget", u"eye_blink", None))
+        self.eye_sad.setText(QCoreApplication.translate("Widget", u"eye_sad", None))
+        self.eye_surprise.setText(QCoreApplication.translate("Widget", u"eye_surprise", None))
+        self.eye_anger.setText(QCoreApplication.translate("Widget", u"eye_anger", None))
+        self.eye_right.setText(QCoreApplication.translate("Widget", u"eye_right", None))
+        self.eye_left.setText(QCoreApplication.translate("Widget", u"eye_left", None))
         self.pushButton_11.setText(QCoreApplication.translate("Widget", u"eye_sad", None))
         self.pushButton_12.setText(QCoreApplication.translate("Widget", u"eye_sad", None))
         self.label.setText(QCoreApplication.translate("Widget", u"\u4e32\u53e3\u9009\u62e9", None))
         self.label_2.setText(QCoreApplication.translate("Widget", u"\u6ce2\u7279\u7387\u9009\u62e9", None))
-        self.pushButton_13.setText(QCoreApplication.translate("Widget", u"open", None))
-        self.pushButton_14.setText(QCoreApplication.translate("Widget", u"close", None))
-        self.pushButton_15.setText(QCoreApplication.translate("Widget", u"Github", None))
+        self.open.setText(QCoreApplication.translate("Widget", u"open", None))
+        self.close.setText(QCoreApplication.translate("Widget", u"close", None))
+        self.github.setText(QCoreApplication.translate("Widget", u"Github", None))
     # retranslateUi
 
