9f8936dd9d541eaa4b8511f526b02056d961a303
NIGHT <3210103847@zju.edu.cn>
2024-10-19 23:53:42 +0800

    initialize

---
+++ readme.md
@@ -0,0 +1,10 @@
+To pack the code into .exe file, run `pyinstaller -F -w --ico=man.ico main.py `
+
+Important:
+You need to run the following command to generate the ui_form.py file
+`pyside6-uic form.ui -o ui_form.py`, or
+`pyside2-uic form.ui -o ui_form.py`
+
+# 
+
+暖色调的浅色
---
+++ ui_form.py
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'form.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QLabel, QListView, QPushButton,
+    QSizePolicy, QTextEdit, QWidget)
+
+class Ui_Widget(object):
+    def setupUi(self, Widget):
+        if not Widget.objectName():
+            Widget.setObjectName(u"Widget")
+        Widget.resize(643, 566)
+        Widget.setStyleSheet(u"background-color: rgb(255, 248, 232);")
+        self.pushButton = QPushButton(Widget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(370, 40, 101, 41))
+        self.pushButton.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+"font: 12pt \"Modern No. 20\";\n"
+"color: rgb(0, 0, 0);")
+        self.pushButton_2 = QPushButton(Widget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_2.setGeometry(QRect(490, 40, 101, 41))
+        self.pushButton_2.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+"font: 12pt \"Modern No. 20\";\n"
+"color: rgb(0, 0, 0);")
+        self.pushButton_3 = QPushButton(Widget)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+        self.pushButton_3.setGeometry(QRect(370, 100, 101, 41))
+        self.pushButton_3.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+"font: 12pt \"Modern No. 20\";\n"
+"color: rgb(0, 0, 0);")
+        self.pushButton_4 = QPushButton(Widget)
+        self.pushButton_4.setObjectName(u"pushButton_4")
+        self.pushButton_4.setGeometry(QRect(490, 100, 101, 41))
+        self.pushButton_4.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+"font: 12pt \"Modern No. 20\";\n"
+"color: rgb(0, 0, 0);")
+        self.listView = QListView(Widget)
+        self.listView.setObjectName(u"listView")
+        self.listView.setGeometry(QRect(340, 20, 281, 141))
+        self.listView.setStyleSheet(u"background-color: rgb(103, 70, 54);")
+        self.listView_2 = QListView(Widget)
+        self.listView_2.setObjectName(u"listView_2")
+        self.listView_2.setGeometry(QRect(340, 180, 281, 151))
+        self.listView_2.setStyleSheet(u"background-color: rgb(177, 148, 112);")
+        self.listView_4 = QListView(Widget)
+        self.listView_4.setObjectName(u"listView_4")
+        self.listView_4.setGeometry(QRect(20, 20, 281, 441))
+        self.listView_4.setStyleSheet(u"background-color: rgb(103, 70, 54);")
+        self.pushButton_5 = QPushButton(Widget)
+        self.pushButton_5.setObjectName(u"pushButton_5")
+        self.pushButton_5.setGeometry(QRect(50, 270, 101, 41))
+        self.pushButton_5.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+"font: 12pt \"Modern No. 20\";\n"
+"color: rgb(0, 0, 0);")
+        self.pushButton_6 = QPushButton(Widget)
+        self.pushButton_6.setObjectName(u"pushButton_6")
+        self.pushButton_6.setGeometry(QRect(170, 270, 101, 41))
+        self.pushButton_6.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+"font: 12pt \"Modern No. 20\";\n"
+"color: rgb(0, 0, 0);")
+        self.pushButton_7 = QPushButton(Widget)
+        self.pushButton_7.setObjectName(u"pushButton_7")
+        self.pushButton_7.setGeometry(QRect(170, 330, 101, 41))
+        self.pushButton_7.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+"font: 12pt \"Modern No. 20\";\n"
+"color: rgb(0, 0, 0);")
+        self.pushButton_8 = QPushButton(Widget)
+        self.pushButton_8.setObjectName(u"pushButton_8")
+        self.pushButton_8.setGeometry(QRect(50, 330, 101, 41))
+        self.pushButton_8.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+"font: 12pt \"Modern No. 20\";\n"
+"color: rgb(0, 0, 0);")
+        self.pushButton_9 = QPushButton(Widget)
+        self.pushButton_9.setObjectName(u"pushButton_9")
+        self.pushButton_9.setGeometry(QRect(50, 390, 101, 41))
+        self.pushButton_9.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+"font: 12pt \"Modern No. 20\";\n"
+"color: rgb(0, 0, 0);")
+        self.pushButton_10 = QPushButton(Widget)
+        self.pushButton_10.setObjectName(u"pushButton_10")
+        self.pushButton_10.setGeometry(QRect(170, 390, 101, 41))
+        self.pushButton_10.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+"font: 12pt \"Modern No. 20\";\n"
+"color: rgb(0, 0, 0);")
+        self.textEdit = QTextEdit(Widget)
+        self.textEdit.setObjectName(u"textEdit")
+        self.textEdit.setGeometry(QRect(340, 350, 281, 111))
+        self.textEdit.setStyleSheet(u"background-color: rgb(160, 147, 125);")
+        self.listView_3 = QListView(Widget)
+        self.listView_3.setObjectName(u"listView_3")
+        self.listView_3.setGeometry(QRect(50, 50, 211, 192))
+        self.listView_3.setStyleSheet(u"border-image: url(:/new/prefix1/robot.png);\n"
+"background-color: rgba(255, 255, 255, 0);")
+        self.pushButton_11 = QPushButton(Widget)
+        self.pushButton_11.setObjectName(u"pushButton_11")
+        self.pushButton_11.setGeometry(QRect(110, 480, 101, 31))
+        self.pushButton_11.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+"font: 12pt \"Modern No. 20\";\n"
+"color: rgb(0, 0, 0);")
+        self.pushButton_12 = QPushButton(Widget)
+        self.pushButton_12.setObjectName(u"pushButton_12")
+        self.pushButton_12.setGeometry(QRect(320, 480, 101, 31))
+        self.pushButton_12.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+"font: 12pt \"Modern No. 20\";\n"
+"color: rgb(0, 0, 0);")
+        self.label = QLabel(Widget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(40, 490, 61, 21))
+        self.label.setStyleSheet(u"font: 10pt \"Microsoft YaHei UI\";\n"
+"color: rgb(0, 0, 0);")
+        self.label_2 = QLabel(Widget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(230, 490, 71, 21))
+        self.label_2.setStyleSheet(u"font: 10pt \"Microsoft YaHei UI\";\n"
+"color: rgb(0, 0, 0);")
+        self.pushButton_13 = QPushButton(Widget)
+        self.pushButton_13.setObjectName(u"pushButton_13")
+        self.pushButton_13.setGeometry(QRect(460, 480, 61, 31))
+        self.pushButton_13.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+"font: 12pt \"Modern No. 20\";\n"
+"color: rgb(0, 0, 0);")
+        self.pushButton_14 = QPushButton(Widget)
+        self.pushButton_14.setObjectName(u"pushButton_14")
+        self.pushButton_14.setGeometry(QRect(560, 480, 61, 31))
+        self.pushButton_14.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+"font: 12pt \"Modern No. 20\";\n"
+"color: rgb(0, 0, 0);")
+        self.pushButton_15 = QPushButton(Widget)
+        self.pushButton_15.setObjectName(u"pushButton_15")
+        self.pushButton_15.setGeometry(QRect(520, 520, 101, 31))
+        self.pushButton_15.setStyleSheet(u"background-color: rgb(247, 238, 211);\n"
+"font: 12pt \"Modern No. 20\";\n"
+"color: rgb(0, 0, 0);")
+        self.listView.raise_()
+        self.pushButton.raise_()
+        self.pushButton_2.raise_()
+        self.pushButton_3.raise_()
+        self.pushButton_4.raise_()
+        self.listView_2.raise_()
+        self.listView_4.raise_()
+        self.pushButton_5.raise_()
+        self.pushButton_6.raise_()
+        self.pushButton_7.raise_()
+        self.pushButton_8.raise_()
+        self.pushButton_9.raise_()
+        self.pushButton_10.raise_()
+        self.textEdit.raise_()
+        self.listView_3.raise_()
+        self.pushButton_11.raise_()
+        self.pushButton_12.raise_()
+        self.label.raise_()
+        self.label_2.raise_()
+        self.pushButton_13.raise_()
+        self.pushButton_14.raise_()
+        self.pushButton_15.raise_()
+
+        self.retranslateUi(Widget)
+
+        QMetaObject.connectSlotsByName(Widget)
+    # setupUi
+
+    def retranslateUi(self, Widget):
+        Widget.setWindowTitle(QCoreApplication.translate("Widget", u"Widget", None))
+        self.pushButton.setText(QCoreApplication.translate("Widget", u"head-center", None))
+        self.pushButton_2.setText(QCoreApplication.translate("Widget", u"head_nod", None))
+        self.pushButton_3.setText(QCoreApplication.translate("Widget", u"head_shake", None))
+        self.pushButton_4.setText(QCoreApplication.translate("Widget", u"head_roll", None))
+        self.pushButton_5.setText(QCoreApplication.translate("Widget", u"eye_blink", None))
+        self.pushButton_6.setText(QCoreApplication.translate("Widget", u"eye_sad", None))
+        self.pushButton_7.setText(QCoreApplication.translate("Widget", u"eye_surprise", None))
+        self.pushButton_8.setText(QCoreApplication.translate("Widget", u"eye_anger", None))
+        self.pushButton_9.setText(QCoreApplication.translate("Widget", u"eye_right", None))
+        self.pushButton_10.setText(QCoreApplication.translate("Widget", u"eye_left", None))
+        self.pushButton_11.setText(QCoreApplication.translate("Widget", u"eye_sad", None))
+        self.pushButton_12.setText(QCoreApplication.translate("Widget", u"eye_sad", None))
+        self.label.setText(QCoreApplication.translate("Widget", u"\u4e32\u53e3\u9009\u62e9", None))
+        self.label_2.setText(QCoreApplication.translate("Widget", u"\u6ce2\u7279\u7387\u9009\u62e9", None))
+        self.pushButton_13.setText(QCoreApplication.translate("Widget", u"open", None))
+        self.pushButton_14.setText(QCoreApplication.translate("Widget", u"close", None))
+        self.pushButton_15.setText(QCoreApplication.translate("Widget", u"Github", None))
+    # retranslateUi
+
